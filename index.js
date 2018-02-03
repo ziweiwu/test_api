4f5dd0076a23a6031a5ce495d89171b92a1a103e
ziwei wu <ziweiwu@gmail.com>
2018-02-03 15:09:25 -0500

    server running

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Particle = require('particle-api-js');
 var particle = new Particle();
 var token;
+const express = require('express');
+const app = express();
 
-particle.login({ username: 'email@example.com', password: 'pass' }).then(
+particle.login({ username: 'alex.toutongi@gmail.com', password: 'Camaro2010' }).then(
     function (data) {
         console.log('API call completed on promise resolve: ', data.body.access_token);
     },
@@ -10,3 +12,27 @@ particle.login({ username: 'email@example.com', password: 'pass' }).then(
         console.log('API call completed on promise fail: ', err);
     }
 );
+
+particle.getVariable({ deviceId: '', name: 'temp', auth: token }).then(function(data) {
+  console.log('Device variable retrieved successfully:', data);
+}, function(err) {
+  console.log('An error occurred while getting attrs:', err);
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
